59719892862d5298e3287e316fec490f5f3036f9
guillermm <guillermm@gmail.com>
2018-11-06 18:08:09 -0500

    Se actualizó a versión Odoo 10

--- __manifest__.py
+++ __manifest__.py
@@ -6,9 +6,15 @@
     'summary': """
         genera la facturacion electronica para la distribucion colombiana segun requisitos de la DIAN""",
     'category': 'Administration',
-    'version': '9.0',
+    'version': '10.0.0.0.1',
     'depends': [
         'account', 'l10n_co_tax_extension',
     ],
-    'data': ['views/dian_view.xml','views/company_view.xml','views/invoice_view.xml','views/sequence_view.xml','views/report_dian_document.xml']
+    'data': [
+    'views/dian_view.xml',
+    'views/company_view.xml',
+    'views/invoice_view.xml',
+    'views/sequence_view.xml',
+    'views/report_dian_document.xml'
+    ]
 }
