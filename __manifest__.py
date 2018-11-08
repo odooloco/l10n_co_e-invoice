b224cc8c45cc6e79cab58fd01c72840eb958a46a
guillermm <guillermm@gmail.com>
2018-11-08 18:07:35 -0500

    Se agregó información

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
-
+# Copyright 2018 Dominic Krimmer, dominic.krimmer@gmail.com
+# Copyright 2018 https://github.com/johnsh, sebastian80_23@hotmail.com
+# Copyright 2018 https://github.com/guillermm, EXA Auto Parts S.A.S
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     'name': "Colombian e-invoice",
-
     'summary': """
-        genera la facturacion electronica para la distribucion colombiana segun requisitos de la DIAN""",
+        Genera la facturación electrónica para la distribución colombiana según requisitos de la DIAN""",
     'category': 'Administration',
     'version': '10.0.0.0.1',
+    'license': 'AGPL-3',
     'depends': [
         'account', 'l10n_co_tax_extension',
     ],
@@ -16,5 +19,6 @@
     'views/invoice_view.xml',
     'views/sequence_view.xml',
     'views/report_dian_document.xml'
-    ]
+    ],
+    'installable': True,
 }
